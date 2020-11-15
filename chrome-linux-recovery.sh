d844b6505c9fb78a193b3bb36519398588ad0433
Vbahole <gooss22@gmail.com>
2020-11-15 18:10:47 -0500

    Update chrome-linux-recovery.sh

--- chrome-linux-recovery.sh
+++ chrome-linux-recovery.sh
@@ -1,3 +1,8 @@
+# script to run right after enabling linux on chromebook
+# paste to the linux files drive
+# chmod +x linux-recovery.sh
+# ./linux-recovery.sh
+
 sudo apt-get update
 sudo apt-get install git
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add
