0aacaa85462b86d541cfcc5d15dc029fec2f552f
Vbahole <gooss22@gmail.com>
2020-11-15 17:45:32 -0500

    Create chrome-linux-recovery.sh

---
+++ chrome-linux-recovery.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install git
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get install atom
