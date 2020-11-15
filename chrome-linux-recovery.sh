0ba65fe4cd6aced802a903d9dc9acb162852c1f1
Vbahole <gooss22@gmail.com>
2020-11-15 18:08:38 -0500

    Update chrome-linux-recovery.sh

--- chrome-linux-recovery.sh
+++ chrome-linux-recovery.sh
@@ -3,5 +3,6 @@ sudo apt-get install git
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get upgrade
+sudo apt-get update
 sudo apt-get install atom
 mkdir projects
